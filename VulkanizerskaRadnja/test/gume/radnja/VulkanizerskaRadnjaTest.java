84375da9ee0671ace01ee781086bdff02c4431d9
urkeev14 <uros.veljkovic1996@gmail.com>
2018-03-25 14:55:50 +0200

    Uveden javadoc za klasu VulkanizerskaRadnjaTest

--- VulkanizerskaRadnja/test/gume/radnja/VulkanizerskaRadnjaTest.java
+++ VulkanizerskaRadnja/test/gume/radnja/VulkanizerskaRadnjaTest.java
@@ -12,6 +12,11 @@ import org.junit.Test;
 
 import gume.AutoGuma;
 
+/**
+ * 
+ * @author Uros Veljkovic
+ *
+ */
 public class VulkanizerskaRadnjaTest {
 
 	private VulkanizerskaRadnja vRadnja;
@@ -19,15 +24,18 @@ public class VulkanizerskaRadnjaTest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		
-		
+
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
-		
+
 	}
 
+	/**
+	 * 
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
 		vRadnja = new VulkanizerskaRadnja();
@@ -40,28 +48,43 @@ public class VulkanizerskaRadnjaTest {
 
 	}
 
+	/**
+	 * 
+	 * @throws Exception
+	 */
 	@After
 	public void tearDown() throws Exception {
 		vRadnja = null;
 		guma = null;
 	}
 
+	/**
+	 * Test metoda za metodu
+	 * {@link gume.radnja.VulkanizerskaRadnja#dodajGumu(gume.AutoGuma)}
+	 */
 	@Test
 	public void testDodajGumu() {
 
-		
 		vRadnja.dodajGumu(guma);
 
 		assertTrue(vRadnja.pronadjiGumu("Michelin").getFirst().equals(guma));
-		//ispravljena greska u metodi pronadjiGumu
+		// ispravljena greska u metodi pronadjiGumu
 
 	}
 
+	/**
+	 * Test metoda za metodu
+	 * {@link gume.radnja.VulkanizerskaRadnja#dodajGumu(gume.AutoGuma)}
+	 */
 	@Test(expected = java.lang.RuntimeException.class)
 	public void testDodajGumuNull() {
 		vRadnja.dodajGumu(null);
 	}
 
+	/**
+	 * Test metoda za metodu
+	 * {@link gume.radnja.VulkanizerskaRadnja#dodajGumu(gume.AutoGuma)}
+	 */
 	@Test(expected = java.lang.RuntimeException.class)
 	public void testDodajGumuPrecnikVanGranice() {
 
@@ -72,72 +95,83 @@ public class VulkanizerskaRadnjaTest {
 
 		vRadnja.dodajGumu(guma);
 	}
-	
+
+	/**
+	 * Test metoda za metodu
+	 * {@link gume.radnja.VulkanizerskaRadnja#dodajGumu(gume.AutoGuma)}
+	 */
 	@Test(expected = java.lang.RuntimeException.class)
 	public void testDodajGumuPostoji() {
 		vRadnja.dodajGumu(guma);
 		vRadnja.dodajGumu(guma);
 	}
-	
 
+	/**
+	 * Test metoda za metodu
+	 * {@link gume.radnja.VulkanizerskaRadnja#pronadjiGumu(String)}
+	 */
 	@Test
 	public void testPronadjiGumu() {
-		
+
 		assertEquals(null, vRadnja.pronadjiGumu(null));
 
 	}
 
-	// NOF - not object of.
+	/**
+	 * Test metoda za metodu
+	 * {@link gume.radnja.VulkanizerskaRadnja#pronadjiGumu(String)}
+	 */
 	@Test
 	public void testPronadjiGumuJednaGuma() {
-		
+
 		AutoGuma guma2 = new AutoGuma();
 		guma2.setMarkaModel("Trayal");
 		guma2.setSirina(156);
 		guma2.setPrecnik(20);
 		guma2.setVisina(40);
-		
+
 		vRadnja.dodajGumu(guma);
 		vRadnja.dodajGumu(guma2);
-		
+
 		assertTrue(vRadnja.pronadjiGumu("Michelin").getLast().equals(guma));
 
 	}
-	
+
+	/**
+	 * Test metoda za metodu
+	 * {@link gume.radnja.VulkanizerskaRadnja#pronadjiGumu(String)}
+	 */
 	@Test
 	public void testPronadjiGumuViseGuma() {
-		
+
 		LinkedList<AutoGuma> pomocnaListaGuma = new LinkedList<>();
-		
+
 		AutoGuma guma2 = new AutoGuma();
 		AutoGuma guma3 = new AutoGuma();
-		
+
 		guma2.setMarkaModel("Michelin");
 		guma2.setSirina(156);
 		guma2.setPrecnik(20);
 		guma2.setVisina(40);
-		
+
 		guma3.setMarkaModel("Michelin");
 		guma3.setSirina(155);
 		guma3.setPrecnik(20);
 		guma3.setVisina(40);
-		
+
 		vRadnja.dodajGumu(guma);
 		vRadnja.dodajGumu(guma2);
 		vRadnja.dodajGumu(guma3);
-		
+
 		pomocnaListaGuma.add(guma);
 		pomocnaListaGuma.add(guma2);
 		pomocnaListaGuma.add(guma3);
-		
+
 		for (int i = 0; i < pomocnaListaGuma.size(); i++) {
-			if(!(vRadnja.pronadjiGumu("Michelin").contains((pomocnaListaGuma.get(i)))))
+			if (!(vRadnja.pronadjiGumu("Michelin").contains((pomocnaListaGuma.get(i)))))
 				fail("U listi se ne nalazi vise guma marke ");
 		}
-		
-		
-		
-		
+
 	}
 
 }
